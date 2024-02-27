5b704dda0131bf0c6622dd6caac0c14655f3b6fc
nil <medranonii@gmail.com>
2024-02-27 19:02:54 +0100

    splash 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,17 +34,23 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.annotation:annotation:1.6.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation(platform("com.google.firebase:firebase-bom:32.7.2"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
 }
--- app/src/main/java/com/example/globusnilomar/MainActivity.kt
+++ app/src/main/java/com/example/globusnilomar/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.globusnilomar
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.example.globusnilomar.ui.login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
+    val DURACIO: Long = 3000;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+        this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        val logo = findViewById<ImageView>(R.id.gifLogo)
+        Glide.with(this).load(R.drawable.dispositivos).into(logo)
+        canviarActivity()
     }
+
+    private fun canviarActivity(){
+        Handler().postDelayed(Runnable {
+          val intent = Intent(this, LoginActivity::class.java)
+          startActivity(intent)
+        }, DURACIO)
+    }
+
 }
