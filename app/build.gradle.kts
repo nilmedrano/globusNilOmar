57030f6782c9631ff514530cd66c14ba5140d574
nil <medranonii@gmail.com>
2024-03-01 19:10:37 +0100

    splash 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,4 +47,5 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation(platform("com.google.firebase:firebase-bom:32.7.2"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
 }
--- app/src/main/java/com/example/globusnilomar/MainActivity.kt
+++ app/src/main/java/com/example/globusnilomar/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.globusnilomar
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
+    val DURACIO: Long = 3000;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+        this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        val logo = findViewById<ImageView>(R.id.gifLogo)
+        Glide.with(this).load(R.drawable.dispositivos).into(logo)
+        canviarActivity()
     }
+
+
+    private fun canviarActivity(){
+        Handler().postDelayed(Runnable {
+            //val intent = Intent(this, LoginActivity::class.java)
+            val intent = Intent(this, LoginRegisterActivity::class.java)
+            startActivity(intent)
+        }, DURACIO)
+    }
+
+
 }
